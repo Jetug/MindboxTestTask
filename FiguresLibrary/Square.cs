5427f33104159558f63abfb4019f65f033d4a892
Jetug <ksergeev01@gmail.com>
2022-06-24 13:19:48 +0300

    6

---
+++ FiguresLibrary/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLibrary
+{
+    public class Square
+    {
+        public double Side { get; }
+
+        public Square(double side)
+        {
+            if (side < 0 )
+                throw new ArgumentException("Длина сторон не может быть меньше нуля");
+
+            Side = side;
+        }
+
+        public double Area => Math.Pow(Side, 2);
+    }
+}
